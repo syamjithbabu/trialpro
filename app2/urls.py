25b957df0d8b4703ef02563b1b9ab62e98520794
syamjithbabu <syamjithbabu2000@gamil.com>
2022-02-23 13:08:07 +0530

    newv

--- app2/urls.py
+++ app2/urls.py
@@ -6,7 +6,7 @@ app_name='app2'
 urlpatterns = [
    # path('adminmain/',views.Admin,name='adminmain'),
    # path('adminlogin',views.Admlog,name='adminlog'),
-   path('adminhome',views.Admhome,name='adminhome'),
+   path('',views.Admhome,name='adminhome'),
    path('adminupload',views.Admup,name='adminup'),
    path('usersprofile',views.Userpro,name='userpro'),
 ]
